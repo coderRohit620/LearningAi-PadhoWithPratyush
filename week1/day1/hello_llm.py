d48ff2bd125fbbc7d9ae679d82c72eed1dcd000f
coderRohit620 <rohitravi62023@gmail.com>
2026-09-06 07:17:25 +0530

    Initial commit

---
+++ week1/day1/hello_llm.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+from groq import Groq
+
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("Api Key Not Working")
+
+client=Groq(api_key=my_api_key)
+model = "openai/gpt-oss-20b"
+role="user"
+prompt= "Do you know padho with pratyush"
+message={
+    "role":role,
+    "content":prompt
+}
+
+messages= [message]
+response=client.chat.completions.create(
+    model=model,messages=messages
+    )
+# print(response)
+print(response.choices[0].message.content)
